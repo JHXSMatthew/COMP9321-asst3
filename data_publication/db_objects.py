fed6e185bbe82561775247c5e7accd10f9f2d03f
chris <chrismiles2156@gmail.com>
2018-05-19 16:36:05 +1000

    bug fix

--- data_publication/db_objects.py
+++ data_publication/db_objects.py
@@ -25,16 +25,29 @@ class Country(Document):
     Renewable_Percentage = ListField(FloatField())
     Fossil_Fuel_Percentage = ListField(FloatField())
 
-    def __init__(self, name, population, co2, ch4, gni, gini, agri_p, renewable_p, fossil_p, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super(Country, self).__init__(*args, **kwargs)
 
-        self.Name = name
-        self.Population = population
-        self.CO2 = co2
-        self.CH4 = ch4
-        self.GNI = gni
-        self.GINI = gini
-        self.Agriculture_Percentage = agri_p
-        self.Renewable_Percentage = renewable_p
-        self.Fossil_Fuel_Percentage = fossil_p
+        # self.Name = name
+        # self.Population = population
+        # self.CO2 = co2
+        # self.CH4 = ch4
+        # self.GNI = gni
+        # self.GINI = gini
+        # self.Agriculture_Percentage = agri_p
+        # self.Renewable_Percentage = renewable_p
+        # self.Fossil_Fuel_Percentage = fossil_p
+
+if __name__ == "__main__":
+    c2 = Country(Name="Caldari")
+
+    c1 = Country(Name="Gallente Federation", Population=[9999999999], CO2=[1000000000], CH4=[10000000], GNI=[123123123123],
+                 GINI=[60], Agriculture_Percentage=[45], Renewable_Percentage=[30], Fossil_Fuel_Percentage=[40])
+    connect("world-bank")
+    c1.save()
+    c2.save()
+
+    connect("world-bank")
+    for t in Country.objects:
+        print(t.Name, t.Population)
 
