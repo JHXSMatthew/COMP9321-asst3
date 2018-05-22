dddf0d0a5f48c151610af776585325e6b4ca078a
WhyK <mcgradyhaha@gmail.com>
2018-05-22 22:41:00 +1000

    GET:  -- all data  -- a country data  -- country list  -- population  -- CO2  -- CH4  -- GNI  -- GINI  -- A_P  -- R_P  -- F_F_P
    POST:
     -- import data into db

--- data_publication/app.py
+++ data_publication/app.py
@@ -1,17 +1,198 @@
 from flask import Flask, jsonify
 from mongoengine import connect
 from flask_pymongo import PyMongo
+from collections import OrderedDict
 
 from data_publication.world_bank import create_countries_list
 
 app = Flask(__name__)
-app.config['MONGO_DBNAME'] = 'world-bank'
-app.config['MONGO_URI'] = 'mongodb://bobsemple:bobsempleis0P@ds233208.mlab.com:33208/world-bank'
+app.config['MONGO_DBNAME'] = 'comp9321_project'
+app.config['MONGO_URI'] = 'mongodb://mcgradyhaha:Mac2813809@ds231360.mlab.com:31360/comp9321_project'
 
 mongo = PyMongo(app)
 
+############################################################### GET METHOD #############################################
 
-@app.route('/countrylist', methods=['POST'])
+@app.route('/data', methods=['GET'])
+def get_all_data():
+    # Return all countries data in MongoDB database
+
+    counties = mongo.db.country
+    output = []
+    for country in counties.find():
+        output.append({
+            'id': country['_id'],
+            'Country': country['Name'],
+            'Population': country['Population'],
+            'CO2': country['CO2'],
+            'CH4': country['CH4'],
+            'GNI': country['GNI'],
+            'GINI': country['GINI'],
+            'Agriculture_Percentage': country['Agriculture_Percentage'],
+            'Renewable_Percentage': country['Renewable_Percentage'],
+            'Fossil_Fuel_Percentage': country['Fossil_Fuel_Percentage']
+        })
+    return jsonify({'result': output})
+
+
+@app.route('/data/<country>', methods=['GET'])
+def get_one_data(country):
+    # Return a specific country data in MongoDB database
+    countries = mongo.db.country
+
+    country = countries.find_one({'Name': country})
+
+    if country:
+        output = {
+            'id': country['_id'],
+            'Country': country['Name'],
+            'Population': country['Population'],
+            'CO2': country['CO2'],
+            'CH4': country['CH4'],
+            'GNI': country['GNI'],
+            'GINI': country['GINI'],
+            'Agriculture_Percentage': country['Agriculture_Percentage'],
+            'Renewable_Percentage': country['Renewable_Percentage'],
+            'Fossil_Fuel_Percentage': country['Fossil_Fuel_Percentage']
+        }
+    else:
+        output = 'No results found!'
+    return jsonify(output)
+
+
+@app.route('/data/countries', methods=['GET'])
+def get_all_countries():
+    # Return all countries in MongoDB database
+    counties = mongo.db.country
+    output = []
+    for country in counties.find():
+        output.append({
+            'id': country['_id'],
+            'Country': country['Name'],
+        })
+    return jsonify({'result': output})
+
+
+@app.route('/data/population', methods=['GET'])
+def get_all_population():
+    # Return all population in MongoDB database
+    counties = mongo.db.country
+    output = []
+    for country in counties.find():
+        output.append({
+            'id': country['_id'],
+            'Country': country['Name'],
+            'Population': country['Population']
+        })
+    return jsonify({'result': output})
+
+
+@app.route('/data/co2', methods=['GET'])
+def get_all_co2():
+    # Return all CO2 in MongoDB database
+    counties = mongo.db.country
+    output = []
+    for country in counties.find():
+        output.append({
+            'id': country['_id'],
+            'Country': country['Name'],
+            'CO2': country['CO2']
+        })
+    return jsonify({'result': output})
+
+
+@app.route('/data/ch4', methods=['GET'])
+def get_all_ch4():
+    # Return all CH4 in MongoDB database
+    counties = mongo.db.country
+    output = []
+    for country in counties.find():
+        output.append({
+            'id': country['_id'],
+            'Country': country['Name'],
+            'CH4': country['CH4']
+
+        })
+    return jsonify({'result': output})
+
+
+@app.route('/data/gni', methods=['GET'])
+def get_all_gni():
+    # Return all population in MongoDB database
+    counties = mongo.db.country
+    output = []
+    for country in counties.find():
+        output.append({
+            'id': country['_id'],
+            'Country': country['Name'],
+            'GNI': country['GNI']
+
+        })
+    return jsonify({'result': output})
+
+
+@app.route('/data/gini', methods=['GET'])
+def get_all_gini():
+    # Return all population in MongoDB database
+    counties = mongo.db.country
+    output = []
+    for country in counties.find():
+        output.append({
+            'id': country['_id'],
+            'Country': country['Name'],
+            'GINI': country['GINI']
+
+        })
+    return jsonify({'result': output})
+
+
+@app.route('/data/a_p', methods=['GET'])
+def get_all_ap():
+    # Return all population in MongoDB database
+    counties = mongo.db.country
+    output = []
+    for country in counties.find():
+        output.append({
+            'id': country['_id'],
+            'Country': country['Name'],
+            'Agriculture_Percentage': country['Agriculture_Percentage']
+        })
+    return jsonify({'result': output})
+
+
+@app.route('/data/r_p', methods=['GET'])
+def get_all_rp():
+    # Return all population in MongoDB database
+    counties = mongo.db.country
+    output = []
+    for country in counties.find():
+        output.append({
+            'id': country['_id'],
+            'Country': country['Name'],
+            'Renewable_Percentage': country['Renewable_Percentage']
+
+        })
+    return jsonify({'result': output})
+
+
+@app.route('/data/f_f_p', methods=['GET'])
+def get_all_ffp():
+    # Return all population in MongoDB database
+    counties = mongo.db.country
+    output = []
+    for country in counties.find():
+        output.append({
+            'id': country['_id'],
+            'Country': country['Name'],
+            'Fossil_Fuel_Percentage': country['Fossil_Fuel_Percentage']
+
+        })
+    return jsonify({'result': output})
+
+
+############################################################### POST METHOD ############################################
+
+@app.route('/data', methods=['POST'])
 def download_data():
     # Get world bank data
     countries = create_countries_list()
@@ -20,40 +201,12 @@ def download_data():
 
     # Add to mongoDB database
     connect(
-        host='mongodb://bobsemple:bobsempleis0P@ds233208.mlab.com:33208/world-bank'
+        host='mongodb://mcgradyhaha:Mac2813809@ds231360.mlab.com:31360/comp9321_project'
     )
     for country in countries:
         country.save()
     return 'Hello World!'
 
-
-@app.route('/countrylist', methods=['GET'])
-def return_all_data():
-    # Return all data in MongoDB database
-    counties = mongo.db.collection_name
-    output = []
-    for q in counties.find():
-        output.append({'name': q.Name})
-    return jsonify(output)
-
-
-@app.route('/countrylist', methods=['GET'])
-def return_country_data():
-    # Return data in MongoDB database for specific country
-
-    return 'Hello World'
-
-
-@app.route('/countrylist/<country>', methods=['GET'])
-def return_one_country(country):
-    countries = mongo.db.collection_name
-    q = countries.find_one({'country': country})
-
-    if q:
-        output = {}
-    else:
-        output ='No results found'
-    return jsonify(output)
 
 if __name__ == '__main__':
     app.run(debug=True)
