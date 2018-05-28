ab619b726fe007887bb808e9510ab65647e2ef71
WhyK <mcgradyhaha@gmail.com>
2018-05-28 20:35:35 +1000

    overall ranking by year updated

--- ranking.py
+++ ranking.py
@@ -2,41 +2,77 @@ import db_objects
 from mongoengine import connect
 
 
+ALL_INDICATORS = ["Population", "CO2", "CH4", "GNI", "GINI", "Agriculture_Percentage",
+                   "Renewable_Percentage", "Fossil_Fuel_Percentage"]
 
-cache_rank = {}
 def get_ranking(year):
+ connect(
+     host='mongodb://mcgradyhaha:Mac2813809@ds231360.mlab.com:31360/comp9321_project'
+ )
 
-  ALL_INDICATORS = ["Population", "CO2", "CH4", "GNI", "GINI", "Agriculture_Percentage",
-                    "Renewable_Percentage", "Fossil_Fuel_Percentage"]
 
-  cty = db_objects.Country.objects
-  for i in cty:
-      pass
-  if year not in cache_rank:
-    ranking = dict()
-    for indicator in ALL_INDICATORS:
-        temp =dict()
-        for country in cty:
-            value_list = country.get_values_list(indicator, year, year)['data']
-            value = 0
-            if len(value_list) == 1:
-                value = value_list[0]
-            if int(value) < 0:
-                continue
-            temp[country['Name']] = value
 
-        sort = sorted(temp.items(), key=lambda x: x[1], reverse=True)
-        rank = dict()
-        for i in range(len(sort)):
-            rank[sort[i][0]] = {indicator : i+1}
-        deepSearch(ranking, rank)
-    cache_rank[year] = ranking
-  return cache_rank[year]
+ ranking = dict()
+ for indicator in ALL_INDICATORS:
+     temp =dict()
+     for country in db_objects.Country.objects:
+         value_list = country.get_values_list(indicator, year, year)['data']
+         value = 0
+         if len(value_list) == 1:
+             value = value_list[0]
+         if int(value) < 0:
+             continue
+         temp[country['Name']] = value
+
+     sort = sorted(temp.items(), key=lambda x: x[1], reverse=True)
+     rank = dict()
+     for i in range(len(sort)):
+         rank[sort[i][0]] = {indicator : i+1}
+     deepSearch(ranking, rank)
+
+ return ranking
+
+def get_overall_ranking(year):
+   connect(
+       host='mongodb://mcgradyhaha:Mac2813809@ds231360.mlab.com:31360/comp9321_project'
+   )
+
+   ranking = dict()
+   for indicator in ALL_INDICATORS:
+       temp = dict()
+       for country in db_objects.Country.objects:
+           value_list = country.get_values_list(indicator, year, year)['data']
+           value = 0
+           if len(value_list) == 1:
+               value = value_list[0]
+           if int(value) < 0:
+               continue
+           temp[country['Name']] = value
+
+       sort = sorted(temp.items(), key=lambda x: x[1], reverse=True)
+       rank = dict()
+       for i in range(len(sort)):
+           rank[sort[i][0]] = [i+1]
+       for key in rank:
+           if key not in ranking:
+               ranking[key] = rank[key]
+           else:
+               ranking[key] += rank[key]
+
+   for country in ranking.keys():
+       i = sum(ranking[country]) / len(ranking[country])
+       ranking[country] = i
+   sort = sorted(ranking.items(), key=lambda x: x[1])
+   all_ranking=dict()
+   for i in range(len(sort)):
+       all_ranking[sort[i][0]] = i + 1
+
+   return all_ranking
+
 
 def deepSearch(dict1, dict2):
-   for key in dict2.keys():
-       if key not in dict1.keys():
-           dict1[key] = dict2[key]
-       else:
-           deepSearch(dict1[key], dict2[key])
-
+  for key in dict2.keys():
+      if key not in dict1.keys():
+          dict1[key] = dict2[key]
+      else:
+          deepSearch(dict1[key], dict2[key])
--- run_publication.py
+++ run_publication.py
@@ -188,13 +188,24 @@ def get_all_data():
 
 @app.route('/analysis/ranking', methods=['GET'])
 def get_ranking_by_year():
-   if 'year' in request.args:
-       year = int(request.args.get('year'))
-   else:
-       return jsonify({'Error' : 'Add year value as request argument'}), 400
+  if 'year' in request.args:
+      year = int(request.args.get('year'))
+  else:
+      return jsonify({'Error' : 'Add year value as request argument'}), 400
 
-   output = ranking.get_ranking(year)
-   return jsonify(output), 200
+  output = ranking.get_ranking(year)
+  return jsonify(output), 200
+
+
+@app.route('/analysis/all_ranking', methods=['GET'])
+def get_all_ranking_by_year():
+  if 'year' in request.args:
+      year = int(request.args.get('year'))
+  else:
+      return jsonify({'Error' : 'Add year value as request argument'}), 400
+
+  output = ranking.get_overall_ranking(year)
+  return jsonify(output), 200
 
 
 @app.route('/api/indicator', methods=['GET'])
